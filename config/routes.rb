67244a78c2a4f0e0b6171d09bf919ff7960dfe89
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-01 17:46:31 +0800

    route fixed

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :items do
     resources :bookings, only: [:create]
-    resources :item_reviews, only: [:new, :create]
   end
   get 'dashboard', to: 'users#dashboard', as: 'dashboard'
-  resources :bookings, only: [:index, :show, :destroy]
+  resources :bookings, only: [:index, :destroy] do
+    resources :item_reviews, only: [:create]
+  end
 end
