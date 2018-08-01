6d6d3466609dc51e2e2c421edb9d0d797299eede
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-01 17:34:33 +0800

    routes added for item reviews

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :items do
     resources :bookings, only: [:create]
+    resources :item_reviews, only: [:new, :create]
   end
   get 'dashboard', to: 'users#dashboard', as: 'dashboard'
   resources :bookings, only: [:index, :show, :destroy]
