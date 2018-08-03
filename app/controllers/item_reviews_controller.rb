9da6722cd2ddfe40bf9c79453ff3d3b894dac1e3
Reed Van Anrooy <reed.vananrooy@gmail.com>
2018-08-03 13:42:23 +0800

    controller defense against hackers

--- app/controllers/item_reviews_controller.rb
+++ app/controllers/item_reviews_controller.rb
@@ -1,9 +1,11 @@
 class ItemReviewsController < ApplicationController
 
   def create
+    @booking = Booking.find(params[:booking_id])
+    redirect_to dashboard_path if @booking.item_review #hacker defense
     @user = current_user
     @item_review = ItemReview.new(item_review_params)
-    @item_review.booking = Booking.find(params[:booking_id])
+    @item_review.booking = @booking
     if @item_review.save
       redirect_to @item_review.booking.item
     else
