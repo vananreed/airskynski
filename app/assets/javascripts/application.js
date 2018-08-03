22719094e46c88935734bc0286aa95e4184dc82b
James Douglas <jamesjimddouglas@hotmail.com>
2018-08-03 16:49:41 +0800

    Home Page, cards and footer. Banner fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,17 @@
 //= require rails-ujs
 //= require_tree .
+
+var $current, $next, $slides = $(".slideshow .slide");
+
+function doSlideShow () {
+  $current = $slides.filter(".slide.current");
+  $next = $current.next(".slide");
+  if ($next.length < 1) {
+    $next = $slides.first();
+  }
+  $slides.removeClass("previous");
+  $current.addClass("previous").removeClass("current");
+  $next.addClass("current");
+  window.setTimeout(doSlideShow, 5000);
+}
+window.setTimeout(doSlideShow, 5000);
