0f00b6fa8dbd8388039a5886352a924081044bb9
Hugo Collado <hugo.collado91@gmail.com>
2018-08-06 13:57:34 +0800

    navbar scrolling fixed

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,7 +3,7 @@ function initUpdateNavbarOnScroll() {
   const navbarItem = document.querySelectorAll('.navbar-wagon-link-white');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= window.innerHeight - 70) {
         navbar.classList.add('navbar-wagon-white');
         navbarItem.forEach(function(item) {
           item.classList.remove('navbar-wagon-link-white');
